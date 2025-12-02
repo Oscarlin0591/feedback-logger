0069d3e2b21972e8db9090ce10e5f82a48446fe7
Oscar Lin <Oscar.Lin@quinnipiac.edu>
2025-12-01 22:27:32 -0500

    Login Page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+function Login() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default Login;
