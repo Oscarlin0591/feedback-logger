1c2fd6f7e489003930f1bc05d6731517829125c0
BradyG318 <160978995+BradyG318@users.noreply.github.com>
2025-12-01 23:54:37 -0500

    LessonPage

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,14 +1,31 @@
 import { useState, useEffect } from "react"
-import { Card, CardBody, CardImg, CardText, CardTitle } from "react-bootstrap"
+import { Card, CardBody, CardImg, CardText, CardTitle, Col, Row } from "react-bootstrap"
 import { Link, useNavigate } from "react-router-dom"
-import styles from './Lesson.module.css';
+import styles from './LessonCard.module.css';
 
-export function LessonCard(props : {img:string, courseTitle:string, courseDescription:string, courseNum:number}) {
+export function LessonCard(props : {
+    img:string, 
+    lessonTitle:string, 
+    lessonDesc:string, 
+    lessonID:number
+}) {
     const navigate = useNavigate();
 
     return (
-        <>
-        
-        </>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <Card className={styles.Card} onClick={() => navigate({pathname: ``/*Insert respective feedback path here*/})}>
+                <div className={styles.lessonRow}>
+                    <CardImg src={props.img} className={styles.lessonImg} />
+                    <div className={styles.textArea}>
+                        <CardTitle className={styles.lessonTitle}>
+                            {props.lessonTitle}
+                        </CardTitle>
+                        <CardText className={styles.lessonDesc}>
+                            {props.lessonDesc}
+                        </CardText>
+                    </div>
+                </div>
+            </Card>
+        </div>
     )
 }
