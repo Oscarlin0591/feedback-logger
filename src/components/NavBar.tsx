1bcdf6792631aa91c96d85264f31bf8d5b53082b
Oscar Lin <Oscar.Lin@quinnipiac.edu>
2025-12-05 00:30:51 -0500

    Profile page complete

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
-import { Container, Navbar, NavbarBrand, NavLink } from "react-bootstrap";
+import { Container, Image, Navbar, NavbarBrand, NavLink } from "react-bootstrap";
 import Logo from "../assets/contact-form.png"
 import styles from './NavBar.module.css';
+import defaultImage from "../../public/default-avatar.jpg"
 
 
 export const NavBar = () => {
@@ -10,9 +11,11 @@ export const NavBar = () => {
             <Container>
                 <NavbarBrand href="/main" className={styles.NavBrand}>
                 <img width='48' height='48' className={styles.logo} src={Logo}/>
+                {/* Brandon, Oscar, and Brady's Feedback Logger */}
+                BOB's Feedback Logger
                 </NavbarBrand>
             </Container>
-            <NavLink className={styles.NavLink} href="profile">Profile</NavLink>
+            <NavLink style={{margin: 'auto 8px'}} href="profile"><Image src={defaultImage} className={styles.icon}/></NavLink>
             <NavLink className={styles.NavLink} href="/" onClick={() => { localStorage.removeItem('role') }}>Log out</NavLink>
         </Navbar>);
     }
